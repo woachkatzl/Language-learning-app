0b93adebf09ed9e8023a466bcf3182babf6c5bc3
woachkatzl <y.v.polonskaya@gmail.com>
2023-11-09 16:36:25 -0500

    added a list of words

---
+++ src/domain/info/wordsList.jsx
@@ -0,0 +1,37 @@
+export const words = [
+  {
+    id: "1",
+    eglish: "to sample",
+    transcription: "[tə ˈsæmpəl]",
+    russian: "попробовать",
+    topic: "vacation",
+  },
+  {
+    id: "2",
+    eglish: "to explore",
+    transcription: "[tə ɪkˈsplɔr]",
+    russian: "исследовать",
+    topic: "vacation",
+  },
+  {
+    id: "3",
+    eglish: "waterfront",
+    transcription: "[ˈwɔtə˞frʌnt]",
+    russian: "береговая линия, набережная",
+    topic: "vacation",
+  },
+  {
+    id: "4",
+    eglish: "sunscreen",
+    transcription: "[ˈsʌn.skriːn]",
+    russian: "солнцезащитный крем",
+    topic: "vacation",
+  },
+  {
+    id: "5",
+    eglish: "starch",
+    transcription: "[[stɑːtʃ]",
+    russian: "крахмал",
+    topic: "food",
+  },
+];
