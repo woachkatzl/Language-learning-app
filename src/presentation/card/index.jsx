81b77960c5dddafac0f462df7491356f24388e19
woachkatzl <y.v.polonskaya@gmail.com>
2023-11-16 14:28:02 -0500

    styled card, added conditional rendering

--- src/presentation/card/index.jsx
+++ src/presentation/card/index.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Стили
 import styles from "./card.module.scss";
 
-function Card() {
-  return <div className={styles.card}></div>;
+function Card(props) {
+  const { word, transcription, translation } = props;
+
+  //Cостояния
+  const [flipped, setFlipped] = useState(false);
+
+  const handleClick = () => {
+    setFlipped(!flipped);
+  };
+
+  return (
+    <div className={styles.card} onClick={handleClick}>
+      <h2 className={styles.word}>{word}</h2>
+      <p>{transcription}</p>
+      {flipped && <p className={styles.translation}>{translation}</p>}
+      <i className={styles.hint}>Нажмите карточку, чтобы увидеть перевод</i>
+    </div>
+  );
 }
+
+Card.defaultProps = {
+  word: "to sample",
+  transcription: "[tə ˈsæmpəl]",
+  translation: "попробовать",
+};
 
 export { Card };
