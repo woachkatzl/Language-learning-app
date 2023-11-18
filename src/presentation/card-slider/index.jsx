aa679f86c6f95732d1fb79f5cf465f14a6368580
woachkatzl <y.v.polonskaya@gmail.com>
2023-11-18 14:58:39 -0500

    added card slider buttons

--- src/presentation/card-slider/index.jsx
+++ src/presentation/card-slider/index.jsx
@@ -2,14 +2,32 @@ import React from "react";
 
 //Компоненты
 import { Card } from "../card";
+import { Button } from "../ui-kit/button";
 
 //Стили
 import styles from "./cardSlider.module.scss";
+import yellowBtn from "../ui-kit/button/styles/yellowBtn.module.scss";
+
+//Изображения
+import rightArrow from "../ui-kit/button/icons/right-arrow.svg";
+import leftArrow from "../ui-kit/button/icons/left-arrow.svg";
 
 function CardSlider() {
   return (
     <div className={styles.container}>
+      <Button
+        type="button"
+        theme={yellowBtn}
+        icon={leftArrow}
+        alt="Previous card button"
+      />
       <Card />
+      <Button
+        type="button"
+        theme={yellowBtn}
+        icon={rightArrow}
+        alt="Next card button"
+      />
     </div>
   );
 }
