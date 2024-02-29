6a2b16656e3ac008ff82ad86e03d3fda50799894
woachkatzl <y.v.polonskaya@gmail.com>
2024-02-29 15:06:22 -0500

    memoized table input component

--- src/presentation/ui-kit/input-field/index.jsx
+++ src/presentation/ui-kit/input-field/index.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { memo } from "react";
 
 //Стили
 import styles from "./tableInput.module.scss";
 import classNames from "classnames";
 
-function TableInput(props) {
+const TableInput = memo(function TableInput(props) {
   const { value, type, isEmpty, ...otherProps } = props;
 
   const input = classNames(styles.formInput, {
     [styles.emptyInput]: isEmpty,
   });
   return <input className={input} type={type} value={value} {...otherProps} />;
-}
+});
 
 TableInput.defaultProps = {
   type: "text",
