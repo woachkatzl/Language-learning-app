2f843befad21aadf4a1a8e897e6942ae25015d28
woachkatzl <y.v.polonskaya@gmail.com>
2024-08-07 10:54:13 -0400

    added a button for new words

--- src/presentation/pages/mainPage/MainPage.jsx
+++ src/presentation/pages/mainPage/MainPage.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 //Компоненты
 import { ControlTable } from "../../components/control-table";
+import { Button } from "../../ui-kit/button";
 
 //Стили
 import styles from "./mainPage.module.scss";
+import greenBtn from "../../ui-kit/button/styles/greenBtn.module.scss";
 
 function MainPage() {
   return (
     <div className={styles.container}>
       <ControlTable />
+      <Button text="Добавить слово" type="button" theme={greenBtn}></Button>
     </div>
   );
 }
